5005783edc00c763e54b109c2a08a132c572f39e
Budiman Putra Jaya <budimanputrajaya2@gmail.com>
2025-08-27 15:22:40 +0700

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,79 +1,82 @@
-const {
-    defineConfig,
-    globalIgnores,
-} = require("eslint/config");
+const { defineConfig, globalIgnores } = require('eslint/config');
 
-const tsParser = require("@typescript-eslint/parser");
-const globals = require("globals");
-const typescriptEslint = require("@typescript-eslint/eslint-plugin");
-const prettier = require("eslint-plugin-prettier");
-const js = require("@eslint/js");
+const tsParser = require('@typescript-eslint/parser');
+const globals = require('globals');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const prettier = require('eslint-plugin-prettier');
+const js = require('@eslint/js');
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require('@eslint/eslintrc');
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-module.exports = defineConfig([{
+module.exports = defineConfig([
+  {
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2020,
-        sourceType: "module",
-        parserOptions: {},
-
-        globals: {
-            ...globals.browser,
-        },
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      parserOptions: {},
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
 
     plugins: {
-        "@typescript-eslint": typescriptEslint,
-        prettier,
+      '@typescript-eslint': typescriptEslint,
+      prettier,
     },
 
     extends: compat.extends(
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
     ),
 
     rules: {
-        "prettier/prettier": "error",
-        "eqeqeq": ["error", "always"],
-        "no-console": "warn",
-        "no-debugger": "error",
-        "curly": ["error", "all"],
-        "no-unused-vars": "off",
+      'prettier/prettier': 'error',
+      eqeqeq: ['error', 'always'],
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
 
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            "argsIgnorePattern": "^_",
-        }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      'no-var': 'error',
+      'prefer-const': 'error',
 
-        "@typescript-eslint/no-explicit-any": "warn",
-        "@typescript-eslint/no-non-null-assertion": "warn",
-        "no-var": "error",
-        "prefer-const": "error",
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          max: 1,
+          maxEOF: 0,
+        },
+      ],
 
-        "no-multiple-empty-lines": ["error", {
-            "max": 1,
-            "maxEOF": 0,
-        }],
-
-        "semi": "off",
-        "quotes": "off",
+      semi: 'off',
+      quotes: 'off',
     },
-}, globalIgnores([
-    "**/node_modules",
-    "**/library",
-    "**/local",
-    "**/temp",
-    "**/build",
-    "**/settings",
-    "**/profiles",
-    "**/configs",
-])]);
+  },
+  globalIgnores([
+    '**/node_modules',
+    '**/library',
+    '**/local',
+    '**/temp',
+    '**/build',
+    '**/settings',
+    '**/profiles',
+    '**/configs',
+  ]),
+]);
