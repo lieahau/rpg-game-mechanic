11b48b9ffe7bf8d7a5995fca410ebf687956841f
Budiman Putra Jaya <budimanputrajaya2@gmail.com>
2025-08-29 14:25:05 +0700

    fix typo

--- assets/scripts/managers/uiManager.ts
+++ assets/scripts/managers/uiManager.ts
@@ -6,8 +6,8 @@ import { SpellButton } from '../components/ui/spellButton';
 import { DamageButton } from '../components/ui/damageButton';
 const { ccclass, property } = _decorator;
 
-@ccclass('uiManager')
-export class uiManager extends Component {
+@ccclass('UIManagers')
+export class UIManagers extends Component {
   @property(PlayerStatsHUD)
   private playerStatsHUD?: PlayerStatsHUD;
 
